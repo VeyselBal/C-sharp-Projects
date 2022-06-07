6872e1e82bda77edacbcb76f10ea140af9f51363
Veysel Bal <93045542+VeyselBal@users.noreply.github.com>
2022-06-07 13:25:21 +0300

    c sharp project 2 commit

---
+++ Project2/Odev2/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace Odev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.renkSecim = new System.Windows.Forms.ColorDialog();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(224, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sonuç:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(65, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SAYILAR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "METOTLAR";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(310, 79);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(47, 13);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "Cevap...";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kutuları Temizle()";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(51, 95);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(42, 219);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Yazıya Çevir()";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Topla()";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(42, 313);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Çarp()";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(42, 360);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 41);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Üs Al()";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(42, 407);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(118, 41);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Faktoriyel()";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(198, 172);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(118, 41);
+            this.button7.TabIndex = 4;
+            this.button7.Text = "Rastgele Sayı()";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(198, 219);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(118, 41);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "Çizgi Çiz (s1,s2,s3,s4)";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(198, 266);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(118, 41);
+            this.button9.TabIndex = 4;
+            this.button9.Text = "Topla (int[] sayılar)";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(198, 313);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(118, 41);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "Formu Renklendir()";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(198, 360);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(118, 88);
+            this.button11.TabIndex = 4;
+            this.button11.Text = "Formu Renklerdir (Renk)";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(353, 172);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(118, 41);
+            this.button12.TabIndex = 4;
+            this.button12.Text = "Üst_Al_2(Taban_Üs)";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(353, 219);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(118, 41);
+            this.button13.TabIndex = 4;
+            this.button13.Text = "İnt_ÜsAl(Taban Üs)";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "1.sayi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "2.sayi";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(592, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ColorDialog renkSecim;
+    }
+}
+
---
+++ Project2/Odev2/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Odev2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           // Paint += button8_Click;
+        }
+        Random rnd = new Random();
+
+        private void kutulariTemizle()
+        {
+            bool durum = false;
+            button1.Visible = button2.Visible = button3.Visible = button4.Visible = button5.Visible =
+            button6.Visible = button7.Visible = button8.Visible = button9.Visible = button10.Visible =
+            button11.Visible = button12.Visible = button13.Visible = durum;
+        }
+        private void yazıyaCevir()
+        {
+            int sayi= Convert.ToInt32(lblSonuc.Text);
+            string[] birler = { "", "BİR", "İKİ", "ÜÇ", "DÖRT", "BEŞ", "ALTI", "YEDİ", "SEKİZ", "DOKUZ" };
+            string[] onlar = { "", "ON", "YİRMİ", "OTUZ", "KIRK", "ELLİ", "ALTMIŞ", "YETMİŞ", "SEKSEN", "DOKSAN" };
+            string[] yuzler = { "", "YÜZ", "İKİYÜZ", "ÜÇYÜZ", "DÖRTYÜZ", "BEŞYÜZ", "ALTIYÜZ", "YEDİYÜZ", "SEKİZYÜZ", "DOKUZYÜZ" };
+
+            if (sayi == 0)
+            {
+                lblSonuc.Text = "SIFIR";
+            }
+            else
+            {
+                lblSonuc.Text = yuzler[sayi / 100] + onlar[(sayi % 100) / 10] + birler[sayi % 10];
+            }
+        }
+        private void topla()
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            int y = Convert.ToInt32(textBox2.Text);
+            lblSonuc.Text = (x + y).ToString();
+        }
+        private void carp()
+        {
+            int x = Convert.ToInt32(textBox1.Text);
+            int y = Convert.ToInt32(textBox2.Text);
+            lblSonuc.Text = (x * y).ToString();
+        }
+
+        private void usAl()
+        {
+            int taban=Convert.ToInt32(textBox1.Text);
+            int us=Convert.ToInt32(textBox2.Text);
+
+            int total = 1;
+            for (int i = 0; i < us; i++)
+            {
+                total *= taban;
+            }
+            lblSonuc.Text = total.ToString();
+        }
+
+        private void faktoriyel()
+        {
+            int n1=Convert.ToInt32(textBox1.Text);
+            int n2=Convert.ToInt32(textBox2.Text);
+            if (n1 > n2)
+            {
+                int temp = n1;
+                n1 = n2;
+                n2 = temp;
+            }
+            int faktoriyell=n2- n1;
+            for (int i = 1; i < n2-n1; i++)
+            {
+                faktoriyell *= i;
+            }
+            MessageBox.Show("2 sayının arasındaki farkın faktoriyeli: "+faktoriyell.ToString());
+        }
+
+        private void rastgeleSayi()
+        {
+            Random rnd=new Random();
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.Text =rnd.Next(1,10).ToString();
+            textBox2.Text =rnd.Next(1,10).ToString();
+        }
+        
+        private void cizgiCiz(int s1,int s2,int s3,int s4)
+        {
+            Graphics graphics=CreateGraphics();
+            Pen pen=new Pen(Color.Blue,7);
+            graphics.DrawLine(pen,s1,s2,s3,s4);
+        }
+        
+        private void topla(int[] sayilar)
+        {
+            int total = 0;
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                total+= sayilar[i];
+            }
+            lblSonuc.Text=total.ToString();
+        }
+
+        private void formuRenklendir()
+        {
+            
+            this.BackColor = Color.FromArgb(rnd.Next(1,255), rnd.Next(1, 255), rnd.Next(1, 255));        
+        }
+
+        private void formuRenklendir(Color renk)
+        {
+             this.BackColor = renk;
+        }
+
+        private void us_al2(double taban,double us)
+        {
+            double total =1;
+            for(int i = 0; i < us; i++)
+            {
+                total *= taban;
+            }
+            lblSonuc.Text=total.ToString();
+        }
+
+        private int intusAl(int taban,int us)
+        {
+            int total = 1;
+            for (int i = 0; i < us; i++)
+            {
+                total *= taban;
+            }
+            return total;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            kutulariTemizle();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            yazıyaCevir();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            topla();
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            carp();
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            usAl();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            faktoriyel();
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            rastgeleSayi();
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int s1 = rnd.Next(0, 500);
+            int s2 = rnd.Next(0, 500);
+            int s3 = rnd.Next(0, 500);
+            int s4 = rnd.Next(0, 500);
+            cizgiCiz(s1, s2, s3, s4);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int n1=Convert.ToInt32(textBox1.Text);
+            int n2=Convert.ToInt32(textBox2.Text);
+            if (n1 > n2)
+            {
+                int temp = n1;
+                n1=n2;
+                n2=temp;
+            }
+            int[] numbers=new int[n2-n1];
+
+            int index = 0;
+            for (int i = n1; i < n2; i++)
+            {
+                numbers[index] = i;
+                index++;
+            }
+            topla(numbers);
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            formuRenklendir();
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult;
+            dialogResult=renkSecim.ShowDialog();
+            Color renk = renkSecim.Color;
+            formuRenklendir(renk);
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            us_al2(Convert.ToDouble(textBox1.Text), 
+                Convert.ToDouble(textBox2.Text));
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            int n1=Convert.ToInt32(textBox1.Text);
+            int n2=Convert.ToInt32(textBox2.Text);
+            lblSonuc.Text = intusAl(n1, n2).ToString();
+        } 
+    }
+}
