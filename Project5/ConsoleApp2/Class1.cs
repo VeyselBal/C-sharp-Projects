ace0dd68cd781bfa12b96fdab0fc8a9e97f6779f
Veysel Bal <93045542+VeyselBal@users.noreply.github.com>
2022-06-15 23:33:44 +0300

    c sharp project 5 commit

---
+++ Project5/ConsoleApp2/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Insan
+    {
+        int boy, kilo, yas;
+
+        public Insan()
+        {
+            yas = 1;
+            Console.WriteLine("bir insan oluşturuldu");
+        }
+
+        public Insan(int boy, int kilo, int yas)
+        {
+            this.boy = boy;
+            this.kilo = kilo;
+            this.yas = yas;
+        }
+
+        public override string ToString()
+        {
+            return "İnsanın boyu: " + boy + " kilo: " + kilo + " yas: " + yas;
+        }
+
+        public void yemek()
+        {
+
+        }
+    }
+    public class Calisan : Insan
+    {
+        double maas;
+
+        public void zam(double oran)
+        {
+            maas = maas + maas * oran;
+        }
+    }
+}
---
+++ Project5/ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Insan insan = new Insan();
+
+            Console.WriteLine(insan.ToString());
+
+            List<Insan> sinif = new List<Insan>();
+
+            Insan insan1 = new Insan(189,45,63);
+            Insan insan2 = new Insan(180,96,87);
+            Insan insan3 = new Insan(175,73,49);
+            Insan insan4 = new Insan(170,45,61);
+            Insan insan5 = new Insan(165,58,57);
+
+            sinif.Add(insan1);
+            sinif.Add(insan2);
+            sinif.Add(insan3);
+            sinif.Add(insan4);
+            sinif.Add(insan5);
+
+            foreach (Insan value in sinif)
+            {
+                Console.WriteLine(value.ToString());
+            }
+            Console.ReadLine();
+        }
+    }
+}
