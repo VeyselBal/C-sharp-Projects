968150a311f561c8db8c2fa0e1854dbae07df3aa
Veysel Bal <93045542+VeyselBal@users.noreply.github.com>
2022-06-15 23:33:12 +0300

    C sharp project 4 commit

---
+++ Project4/Project4/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(comboBox1.Text) && !string.IsNullOrEmpty(comboBox2.Text) && 
+                !string.IsNullOrEmpty(comboBox3.Text) && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                int number=Convert.ToInt32(textBox1.Text);
+                if (number <45 || number > 50)
+                {
+                    MessageBox.Show("koltuk Sayısı Geçerli Değil!!! ");
+                }
+                else
+                {
+                    Otobus otobus = new Otobus(comboBox1.Text, comboBox2.Text, comboBox3.Text, number);
+
+                    listBox1.Items.Add("------------------------------------------------------");
+                    listBox1.Items.Add("Sefer adı:          "+ otobus.SeferAdi);
+                    listBox1.Items.Add("Sefer saati:       " + otobus.SeferSaati);
+                    listBox1.Items.Add("Model:              " + otobus.Model);
+                    listBox1.Items.Add("Koltuk sayısı:     " + otobus.KoltukSayisi);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid input", "Invailid Input");
+            }
+        }
+    }
+}
---
+++ Project4/Project4/Otobus.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project4
+{
+    public class Otobus
+    {
+        string seferAdi;
+        string seferSaati;
+        string model;
+        int koltukSayisi;
+
+        public Otobus(string seferAdi, string seferSaati, string model, int koltukSayisi)
+        {
+            this.seferAdi = seferAdi;
+            this.seferSaati = seferSaati;   
+            this.model = model; 
+            this.koltukSayisi = koltukSayisi;
+        }
+        public string SeferAdi { get { return seferAdi; } set { seferAdi = value; } }
+        public string SeferSaati { get { return seferSaati; } set { seferSaati = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int KoltukSayisi { get { return koltukSayisi; } set { koltukSayisi = value; } }
+    }
+}
